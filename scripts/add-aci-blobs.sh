c3995e278e1e9d8ce391ce59d169f9522fbffe4b
Amit Bose <amitbose@gmail.com>
2017-08-09 11:40:34 -0700

    Install controller, host-agent and CNI
    Signed-off-by: Amit Bose <amitbose@gmail.com>

--- scripts/add-aci-blobs.sh
+++ scripts/add-aci-blobs.sh
@@ -7,11 +7,10 @@ bosh add-blob ${DIR}/aci-containers-controller aci-cf-controller/aci-containers-
 bosh add-blob ${DIR}/aci-containers-host-agent aci-cf-host-agent/aci-containers-host-agent
 bosh add-blob ${DIR}/ovsresync aci-cf-host-agent/ovsresync
 
-bosh add-blob ${DIR}/agent-ovs_1.3.0*~trusty_amd64.deb agent-ovs/agent-ovs_1.3.0~trusty_amd64.deb
-bosh add-blob ${DIR}/libmodelgbp_1.3.0*~trusty_amd64.deb agent-ovs/libmodelgbp_1.3.0~trusty_amd64.deb
-bosh add-blob ${DIR}/libopflex_1.3.0*~trusty_amd64.deb agent-ovs/libopflex_1.3.0~trusty_amd64.deb
+bosh add-blob ${DIR}/agent-ovs_1.5.0*~trusty_amd64.deb agent-ovs/agent-ovs_1.5.0~trusty_amd64.deb
+bosh add-blob ${DIR}/libmodelgbp_1.5.0*~trusty_amd64.deb agent-ovs/libmodelgbp_1.5.0~trusty_amd64.deb
+bosh add-blob ${DIR}/libopflex_1.5.0*~trusty_amd64.deb agent-ovs/libopflex_1.5.0~trusty_amd64.deb
 bosh add-blob ${DIR}/libuv1_1.8.0*~trusty_amd64.deb agent-ovs/libuv1_1.8.0~trusty_amd64.deb
-bosh add-blob ${DIR}/openvswitch-lib_2.4.1~gbp*_amd64.deb agent-ovs/openvswitch-lib_2.4.1~gbp_amd64.deb
 
 bosh add-blob ${DIR}/opflex-agent-cni opflex-cni/opflex-agent-cni
 
