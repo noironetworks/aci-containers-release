e8f595ad810a299d1fcf9cf563fe0b085ff882ab
Amit Bose <amitbose@gmail.com>
2018-02-05 15:33:48 -0800

    Update agent-ovs version number
    Signed-off-by: Amit Bose <amitbose@gmail.com>

--- scripts-internal/add-aci-blobs.sh
+++ scripts-internal/add-aci-blobs.sh
@@ -6,9 +6,9 @@ bosh add-blob ${DIR}/aci-containers-controller_1.8.0~trusty*_amd64.deb aci-cf-co
 bosh add-blob ${DIR}/aci-containers-host-agent_1.8.0~trusty*_amd64.deb aci-cf-host-agent/aci-containers-host-agent_1.8.0~trusty_amd64.deb
 bosh add-blob ${DIR}/opflex-agent-cni_1.8.0~trusty*_amd64.deb opflex-cni/opflex-agent-cni_1.8.0~trusty_amd64.deb
 
-bosh add-blob ${DIR}/agent-ovs_1.5.0*~trusty_amd64.deb agent-ovs/agent-ovs_1.5.0~trusty_amd64.deb
-bosh add-blob ${DIR}/libmodelgbp_1.5.0*~trusty_amd64.deb agent-ovs/libmodelgbp_1.5.0~trusty_amd64.deb
-bosh add-blob ${DIR}/libopflex_1.5.0*~trusty_amd64.deb agent-ovs/libopflex_1.5.0~trusty_amd64.deb
+bosh add-blob ${DIR}/agent-ovs_1.6.0*~trusty_amd64.deb agent-ovs/agent-ovs_1.6.0~trusty_amd64.deb
+bosh add-blob ${DIR}/libmodelgbp_1.6.0*~trusty_amd64.deb agent-ovs/libmodelgbp_1.6.0~trusty_amd64.deb
+bosh add-blob ${DIR}/libopflex_1.6.0*~trusty_amd64.deb agent-ovs/libopflex_1.6.0~trusty_amd64.deb
 bosh add-blob ${DIR}/libuv1_1.8.0*~trusty_amd64.deb agent-ovs/libuv1_1.8.0~trusty_amd64.deb
 
 bosh add-blob ${DIR}/openvswitch-common_2.6.0*_amd64.deb ovs_2.6.0/openvswitch-common_2.6.0_amd64.deb
